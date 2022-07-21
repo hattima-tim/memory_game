8e13ae7ccda77d2dd0f3ea0bd79f3e1fe6102727
hattima-tim <irfanridwan20@gmail.com>
2022-07-21 13:49:54 +0000

    add score states and pass them to cards component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import Cards from "./components/cards";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(3);
   return (
     <>
       <h1>Memory Card Game</h1>
-      <Cards />
+      <Cards
+        score={score}
+        setScore={setScore}
+        bestScore={bestScore}
+        setBestScore={setBestScore}
+      />
     </>
   );
 }
