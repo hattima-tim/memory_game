9a8df64677483f1c74f06922fd2d548d86723cf9
hattima-tim <irfanridwan20@gmail.com>
2022-07-21 16:37:33 +0000

    add logic for randomly showing cards

--- src/components/cards.js
+++ src/components/cards.js
@@ -7,6 +7,7 @@ function Cards({ score, setScore, bestScore, setBestScore }) {
     "https://res.cloudinary.com/du3oueesv/image/upload/v1658404174/images_w2njnz.png";
   const img3Src =
     "https://res.cloudinary.com/du3oueesv/image/upload/v1658404147/images_wabqw3.png";
+
   const primaryImgContainerArr = [
     [img1Src, 1],
     [img2Src, 2],
@@ -14,10 +15,25 @@ function Cards({ score, setScore, bestScore, setBestScore }) {
   ];
   const [images, setImages] = useState(primaryImgContainerArr);
   const [clickedImageIdList, setClickedImageIdList] = useState([]);
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+      // swap elements array[i] and array[j]
+      // we use "destructuring assignment" syntax to achieve that
+      // same can be written as:
+      // let t = array[i]; array[i] = array[j]; array[j] = t
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
   const handleClick = (e) => {
     const isImgAlreadyClicked = clickedImageIdList.find((imgId) => {
       return imgId === e.target.id;
     });
+
     if (isImgAlreadyClicked) {
       setBestScore(score);
       setScore(0);
@@ -27,9 +43,13 @@ function Cards({ score, setScore, bestScore, setBestScore }) {
       setScore((prevScore) => {
         return prevScore + 1;
       });
+
       setClickedImageIdList((prevList) => {
         return [...prevList, e.target.id];
       });
+
+      const shuffledArray = shuffleArray(primaryImgContainerArr);
+      setImages(shuffleArray);
     }
   };
   const imgList = images.map((imgArr) => {
