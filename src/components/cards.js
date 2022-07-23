2cf49bc45b9bbca2790087c29766c76e79f1b122
hattima-tim <irfanridwan20@gmail.com>
2022-07-23 16:00:07 +0000

    add jsx for creating cards

---
+++ src/components/cards.js
@@ -0,0 +1,228 @@
+import "../cardStyle.css";
+function cards({ images }) {
+  return (
+    <div className="cards">
+      <div className="card">
+        <img src={images[0].imgSrc} id={images[0].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[0].name}</b>
+          </h4>
+          <p>{images[0].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[1].imgSrc} id={images[1].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[1].name}</b>
+          </h4>
+          <p>{images[1].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[2].imgSrc} id={images[2].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[2].name}</b>
+          </h4>
+          <p>{images[2].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[3].imgSrc} id={images[3].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[3].name}</b>
+          </h4>
+          <p>{images[3].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[4].imgSrc} id={images[4].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[4].name}</b>
+          </h4>
+          <p>{images[4].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[5].imgSrc} id={images[5].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[5].name}</b>
+          </h4>
+          <p>{images[5].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[6].imgSrc} id={images[6].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[6].name}</b>
+          </h4>
+          <p>{images[6].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[7].imgSrc} id={images[7].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[7].name}</b>
+          </h4>
+          <p>{images[7].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[8].imgSrc} id={images[8].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[8].name}</b>
+          </h4>
+          <p>{images[8].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[9].imgSrc} id={images[9].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[9].name}</b>
+          </h4>
+          <p>{images[9].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[10].imgSrc} id={images[10].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[10].name}</b>
+          </h4>
+          <p>{images[10].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[11].imgSrc} id={images[11].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[11].name}</b>
+          </h4>
+          <p>{images[11].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[12].imgSrc} id={images[12].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[12].name}</b>
+          </h4>
+          <p>{images[12].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[13].imgSrc} id={images[13].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[13].name}</b>
+          </h4>
+          <p>{images[13].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[14].imgSrc} id={images[14].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[14].name}</b>
+          </h4>
+          <p>{images[14].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[15].imgSrc} id={images[15].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[15].name}</b>
+          </h4>
+          <p>{images[15].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[16].imgSrc} id={images[16].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[16].name}</b>
+          </h4>
+          <p>{images[16].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[17].imgSrc} id={images[17].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[17].name}</b>
+          </h4>
+          <p>{images[17].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[18].imgSrc} id={images[18].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[18].name}</b>
+          </h4>
+          <p>{images[18].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[19].imgSrc} id={images[19].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[19].name}</b>
+          </h4>
+          <p>{images[19].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[20].imgSrc} id={images[20].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[20].name}</b>
+          </h4>
+          <p>{images[20].details}</p>
+        </div>
+      </div>
+
+      <div className="card">
+        <img src={images[21].imgSrc} id={images[21].id} alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>{images[21].name}</b>
+          </h4>
+          <p>{images[21].details}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default cards;
