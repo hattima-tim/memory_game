43d62cc440b99836c7f4e5d59eda03bfa49a770c
hattima-tim <irfanridwan20@gmail.com>
2022-07-23 14:17:03 +0000

    import images

--- src/components/cardsLogic.js
+++ src/components/cardsLogic.js
@@ -1,18 +1,6 @@
 import { useState } from "react";
-import uniqid from "uniqid";
+import primaryImgContainerArr from "../images";
 function Cards({ score, setScore, bestScore, setBestScore }) {
-  const img1Src =
-    "https://res.cloudinary.com/du3oueesv/image/upload/v1658404199/images_pl6mlg.png";
-  const img2Src =
-    "https://res.cloudinary.com/du3oueesv/image/upload/v1658404174/images_w2njnz.png";
-  const img3Src =
-    "https://res.cloudinary.com/du3oueesv/image/upload/v1658404147/images_wabqw3.png";
-
-  const primaryImgContainerArr = [
-    [img1Src, 1],
-    [img2Src, 2],
-    [img3Src, 3],
-  ];
   const [images, setImages] = useState(primaryImgContainerArr);
   const [clickedImageIdList, setClickedImageIdList] = useState([]);
 
