9c1f13b4fbec0434e2264380833358e5d652b517
hattima-tim <irfanridwan20@gmail.com>
2022-07-23 14:04:46 +0000

    add images and image infos for cards

---
+++ src/images.js
@@ -0,0 +1,136 @@
+const primaryImgContainerArr = [
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505792/signal-2022-07-22-21-30-39-110_subnr5.png",
+    id: 1,
+    name: "Close-up of a fly",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505793/signal-2022-07-22-21-31-04-839_d9xljn.png",
+    id: 2,
+    name: "Fan-throated lizard",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505802/signal-2022-07-22-21-38-48-968_lqakas.png",
+    id: 3,
+    name: "Eastern bluebirds",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505800/signal-2022-07-22-21-40-01-087_zygsew.png",
+    id: 4,
+    name: "Pine grosbeak",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505792/signal-2022-07-22-21-28-01-904_hcbays.png",
+    id: 5,
+    name: "Two hoatzins",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505789/signal-2022-07-22-21-27-32-800_er6aeo.jpg",
+    id: 6,
+    name: "Brown leghorn rooster",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505792/signal-2022-07-22-21-24-55-052_s6n8ig.jpg",
+    id: 7,
+    name: "Blue-and-yellow macaws",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505796/signal-2022-07-22-21-33-31-835_qhjfhl.png",
+    id: 8,
+    name: "A crested patridge",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505800/signal-2022-07-22-21-40-31-471_v8lyrt.png",
+    id: 9,
+    name: "Sleeping Arctic fox",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505797/signal-2022-07-22-21-37-38-845_sjrdol.png",
+    id: 10,
+    name: "Arctic wolf family",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505800/signal-2022-07-22-21-39-28-131_mrryfy.png",
+    id: 11,
+    name: "Northern lights and wild reindeer",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505792/signal-2022-07-22-21-29-41-860_ah2m6v.png",
+    id: 12,
+    name: "West caucasian tur father and kid",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505789/signal-2022-07-22-21-28-59-365_ynbnqb.jpg",
+    id: 13,
+    name: "A muskcox and her calf",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505789/signal-2022-07-22-21-22-32-498_e9hof3.jpg",
+    id: 14,
+    name: "A female wood duck and ducklings",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505796/signal-2022-07-22-21-32-36-497_kxng0y.png",
+    id: 15,
+    name: "Baby and juvenile elephants",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658505804/signal-2022-07-22-21-42-15-870_o8bvuu.png",
+    id: 16,
+    name: "Lion cubs",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658575998/signal-2022-07-23-15-00-29-735_vnkhom.png",
+    id: 17,
+    name: "Two lion cubs wrestling",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658576000/signal-2022-07-23-15-06-01-868_oy9qdx.png",
+    id: 18,
+    name: "Hamerkop standing on a hippo",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658575999/signal-2022-07-23-15-02-57-034_nmkxgv.png",
+    id: 19,
+    name: "Green sea turtle with sardins",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658575999/signal-2022-07-23-15-08-18-541_q5zefd.png",
+    id: 20,
+    name: "Eurasian red squirrel",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658575999/signal-2022-07-23-15-11-20-146_yezpeq.png",
+    id: 21,
+    name: "A rabbit in the grass",
+  },
+  {
+    imgSrc:
+      "https://res.cloudinary.com/du3oueesv/image/upload/v1658575998/signal-2022-07-23-15-10-01-093_lsumzt.png",
+    id: 22,
+    name: "Octopus spotted during a night dive",
+  },
+];
+
+export default primaryImgContainerArr;
