064bb8a70fb811d7aca50d84d14e56e2d5c599fb
hattima-tim <irfanridwan20@gmail.com>
2022-07-24 02:14:06 +0000

    initialize bestScore state with zero

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CardsLogic from "./components/cardsLogic";
 import "./App.css";
 function App() {
   const [score, setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(3);
+  const [bestScore, setBestScore] = useState(0);
   return (
     <>
       <h1>Memory Card Game</h1>
