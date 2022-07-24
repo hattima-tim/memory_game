089e3289472da801d426409992388d645865fe38
hattima-tim <irfanridwan20@gmail.com>
2022-07-24 02:11:20 +0000

    add jsx for showing scores

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import CardsLogic from "./components/cardsLogic";
-import "./App.css"
+import "./App.css";
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(3);
   return (
     <>
       <h1>Memory Card Game</h1>
+      <div class="score">
+        <p>{`Score: ${score}`}</p>
+        <p>Total Images: 22</p>
+        <p>{`Best Score: ${bestScore}`}</p>
+      </div>
       <CardsLogic
         score={score}
         setScore={setScore}
